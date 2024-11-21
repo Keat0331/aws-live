453729c07bee54e0b6319aee0a5acba175df9120
Keat0331 <125840626+Keat0331@users.noreply.github.com>
2024-11-21 12:52:29 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.ctmc0mawkqa6.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "nginnkeat-employee"
+customregion = "us-east-1"
+
